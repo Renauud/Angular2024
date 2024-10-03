ff3b72f0cb6597d73ec1297cbe58e3fef10b463e
renaudLapTop <rzell57@gmail.com>
2024-10-03 14:12:25 +0200

    creation de film list/modif/new + home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppComponent } from './app.component';
 import { FilmComponent } from './film/film.component';
 import { FormsModule } from '@angular/forms';
 import { FilmService } from './services/film/film.service';
+import { FilmModifComponent } from './film-modif/film-modif.component';
+import { FilmNewComponent } from './film-new/film-new.component';
+import { HomeComponent } from './home/home.component';
+import { FilmListComponent } from './film-list/film-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FilmComponent,
+    FilmModifComponent,
+    FilmNewComponent,
+    HomeComponent,
+    FilmListComponent,
   ],
   imports: [
     BrowserModule,
